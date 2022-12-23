b0c96dddc106894e3c6e654c013d56e1362bc4f5
Hasnain Raza <hasnainraza1072@gmail.com>
2022-12-23 20:52:57 +0530

    add loop functionality

---
+++ ep7/shell.py
@@ -0,0 +1,12 @@
+import basic
+
+while True:
+    text = input('basic > ')
+
+    # get parsed tokens and error (if any)
+    result, error = basic.run('<stdin>', text)
+
+    if error:
+        print(error.as_string())
+    elif result:
+        print(result)
